353bc9e9701712bfcb8ba268b628f67ee1a27f2e
Leo <leonard@trunkrs.nl>
2020-08-03 00:19:16 +0200

    Fix: spotless

--- src/test/java/com/trunkrs/sdk/param/APIV1WebHookParamsTest.java
+++ src/test/java/com/trunkrs/sdk/param/APIV1WebHookParamsTest.java
@@ -1,14 +1,13 @@
 package com.trunkrs.sdk.param;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.trunkrs.sdk.enumeration.EventType;
 import com.trunkrs.sdk.testing.APIV1BaseTest;
 import com.trunkrs.sdk.testing.mocks.Mocks;
-
 import lombok.val;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("V1 - WebHookParams")
 public class APIV1WebHookParamsTest extends APIV1BaseTest {
@@ -27,48 +26,40 @@ public class APIV1WebHookParamsTest extends APIV1BaseTest {
     val headerName = Mocks.faker.internet().slug();
     val sessionToken = Mocks.faker.crypto().sha256();
 
-    val params = WebHookParams.builder()
-      .callbackUrl(callbackURL)
-      .headerName(headerName)
-      .sessionToken(sessionToken)
-      .build();
+    val params =
+        WebHookParams.builder()
+            .callbackUrl(callbackURL)
+            .headerName(headerName)
+            .sessionToken(sessionToken)
+            .build();
 
     assertThat(params)
-      .hasFieldOrPropertyWithValue("callbackUrl", callbackURL)
-      .hasFieldOrPropertyWithValue("sessionHeaderName", headerName)
-      .hasFieldOrPropertyWithValue("sessionToken", sessionToken);
+        .hasFieldOrPropertyWithValue("callbackUrl", callbackURL)
+        .hasFieldOrPropertyWithValue("sessionHeaderName", headerName)
+        .hasFieldOrPropertyWithValue("sessionToken", sessionToken);
   }
 
   @Test
   @DisplayName("Should apply on creation event")
   public void appliesOnCreationEvent() {
-    val params = WebHookParams.builder()
-      .event(EventType.onCreation)
-      .build();
+    val params = WebHookParams.builder().event(EventType.onCreation).build();
 
-    assertThat(params)
-      .hasFieldOrPropertyWithValue("onCreation", true);
+    assertThat(params).hasFieldOrPropertyWithValue("onCreation", true);
   }
 
   @Test
   @DisplayName("Should apply on state update event")
   public void appliesOnStateUpdateEvent() {
-    val params = WebHookParams.builder()
-      .event(EventType.onStateUpdate)
-      .build();
+    val params = WebHookParams.builder().event(EventType.onStateUpdate).build();
 
-    assertThat(params)
-      .hasFieldOrPropertyWithValue("onStatusUpdate", true);
+    assertThat(params).hasFieldOrPropertyWithValue("onStatusUpdate", true);
   }
 
   @Test
   @DisplayName("Should apply on cancellation event")
   public void appliesOnCancellationEvent() {
-    val params = WebHookParams.builder()
-      .event(EventType.onCancellation)
-      .build();
+    val params = WebHookParams.builder().event(EventType.onCancellation).build();
 
-    assertThat(params)
-      .hasFieldOrPropertyWithValue("onCancellation", true);
+    assertThat(params).hasFieldOrPropertyWithValue("onCancellation", true);
   }
 }
--- src/test/java/com/trunkrs/sdk/testing/mocks/Mocks.java
+++ src/test/java/com/trunkrs/sdk/testing/mocks/Mocks.java
@@ -9,26 +9,26 @@ public class Mocks {
 
   public static AddressParams getFakeAddress() {
     return AddressParams.builder()
-      .companyName(faker.company().name())
-      .contactName(faker.overwatch().hero())
-      .addressLine(faker.address().streetAddress())
-      .postalCode(faker.address().zipCode())
-      .city(faker.address().city())
-      .countryCode(faker.address().countryCode())
-      .phoneNumber(faker.phoneNumber().phoneNumber())
-      .email(faker.internet().emailAddress())
-      .remarks(faker.overwatch().quote())
-      .build();
+        .companyName(faker.company().name())
+        .contactName(faker.overwatch().hero())
+        .addressLine(faker.address().streetAddress())
+        .postalCode(faker.address().zipCode())
+        .city(faker.address().city())
+        .countryCode(faker.address().countryCode())
+        .phoneNumber(faker.phoneNumber().phoneNumber())
+        .email(faker.internet().emailAddress())
+        .remarks(faker.overwatch().quote())
+        .build();
   }
 
   public static ParcelParams getFakeParcel() {
     return ParcelParams.builder()
-      .reference(faker.code().ean13())
-      .description(faker.commerce().productName())
-      .height(faker.random().nextInt(20, 200))
-      .width(faker.random().nextInt(20, 200))
-      .depth(faker.random().nextInt(20, 200))
-      .weight(faker.random().nextInt(100, 10000))
-      .build();
+        .reference(faker.code().ean13())
+        .description(faker.commerce().productName())
+        .height(faker.random().nextInt(20, 200))
+        .width(faker.random().nextInt(20, 200))
+        .depth(faker.random().nextInt(20, 200))
+        .weight(faker.random().nextInt(100, 10000))
+        .build();
   }
 }
